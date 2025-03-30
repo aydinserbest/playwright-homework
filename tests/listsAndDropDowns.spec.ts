7fd3bce64a2eb9159f1017b88a73371bfc0290fc
aydinserbest <aydinserbest34@gmail.com>
2025-03-30 23:40:18 +0200

    implement test cases for selecting and updating pet types

---
+++ tests/listsAndDropDowns.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+})
+test('Validate selected pet types from the list', async ({ page }) => {
+    await page.getByRole('button', {name: 'Owners'}).click()
+    await page.getByRole('link', {name: ' Search'}).click()
+    await expect(page.getByRole('heading')).toHaveText('Owners')
+    await page.getByRole('link', {name: 'George Franklin'}).click()
+    await expect(page.locator('b.ownerFullName')).toHaveText('George Franklin')
+    await page.locator('app-pet-list', {hasText: 'Leo'}).getByRole('button', {name: 'Edit Pet'}).click()
+    await expect(page.getByRole('heading')).toHaveText(' Pet ')
+    await expect(page.locator('#owner_name')).toHaveValue('George Franklin')
+    const typeSelectField = page.getByLabel('Type')
+    await expect(typeSelectField).toHaveValue('cat')
+    const typeOptions = await typeSelectField.locator('option').allTextContents()
+
+    for (const option of typeOptions) {
+        await typeSelectField.selectOption(option)
+        await expect(typeSelectField).toHaveValue(option)
+    }    
+})
+test('Validate the pet type update', async ({ page }) => {
+    await page.getByRole('button', {name: 'Owners'}).click()
+    await page.getByRole('link', {name: ' Search'}).click()
+    await expect(page.getByRole('heading')).toHaveText('Owners')
+    await page.getByRole('link', {name: 'Eduardo Rodriquez'}).click()
+    const rosyPetDetails = page.locator('app-pet-list', { hasText: 'Rosy' })
+    await rosyPetDetails.getByRole('button', {name: 'Edit Pet'}).click()
+    await expect(page.getByLabel('Name')).toHaveValue('Rosy')
+    const typeSelectField = page.getByLabel('Type')
+    await expect(typeSelectField).toHaveValue('dog')
+    await typeSelectField.selectOption('bird')
+    await expect(page.locator('#type1')).toHaveValue('bird')
+    await expect(typeSelectField).toHaveValue('bird')
+    await page.getByRole('button', {name: 'Update'}).click()
+    await expect(rosyPetDetails.locator('dd').nth(2)).toHaveText('bird')
+    await rosyPetDetails.getByRole('button', {name: 'Edit Pet'}).click()
+    await expect(typeSelectField).toHaveValue('bird')
+    await typeSelectField.selectOption('dog')
+    await expect(page.locator('#type1')).toHaveValue('dog')
+    await expect(typeSelectField).toHaveValue('dog')
+    await page.getByRole('button', {name: 'Update'}).click()
+    await expect(rosyPetDetails.locator('dd').nth(2)).toHaveText('dog')
+})
