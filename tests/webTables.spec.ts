8a28412819a2ff80d6c36e1777bdd287858a6963
aydinserbest <aydinserbest34@gmail.com>
2025-04-16 17:43:28 +0200

    replaced for-loop method in Validate search by Last Name test

--- tests/webTables.spec.ts
+++ tests/webTables.spec.ts
@@ -18,38 +18,39 @@ test.describe('Owners table validations', () => {
     await expect(page.getByRole('row', { name: 'Madison' })).toHaveCount(4)
   })
   test('Validate search by Last Name', async ({ page }) => {
-    const lastNamesToCheck = ['Black', 'Davis', 'Es']
+    const lastNamesToCheck = ['Black', 'Davis', 'Es', 'Playwright']
 
     for (const lastName of lastNamesToCheck) {
       await page.locator('#lastName').fill(lastName)
       await page.getByRole('button', { name: 'Find Owner' }).click()
       await page.waitForResponse(`**/api/owners?lastName=${lastName}`)
-      const targetRows = page.locator('tbody > tr')
-      for(const row of await targetRows.all()) {
-      await expect(row.locator('td').first()).toContainText(lastName)
+      if (lastName == 'Playwright'){
+        await expect(page.getByText('No owners with LastName starting with "Playwright"')).toBeVisible()
+      } else {
+        for (const row of await page.locator('tbody > tr').all()) {
+          await expect(row.locator('td').first()).toContainText(lastName)
+        }
+      }
     }
-  }
-    await page.locator('#lastName').fill('Playwright')
-    await page.getByRole('button', { name: 'Find Owner' }).click()
-    await expect(page.getByText('No owners with LastName starting with "Playwright"')).toBeVisible()
   })
   test('Validate phone number and pet name on the Owner Information page', async ({ page }) => {
     const rowByPhone = page.getByRole('row', { name: '6085552765' })
-    const petNameField = await rowByPhone.getByRole('row', { name: ' George ' }).textContent()!
+    const petNameField = await rowByPhone.getByRole('row', { name: ' George ' }).textContent()
     await rowByPhone.getByRole('link').click()
     await expect(page.getByRole('row', { name: 'Telephone' }).locator('td')).toHaveText('6085552765')
-    await expect(page.locator('dd').first()).toHaveText(petNameField)
+    await expect(page.locator('dd').first()).toHaveText(petNameField!)
   })
   test('Validate pets of the Madison city', async ({ page }) => {
     await page.waitForResponse('**/api/owners')
     const expectedPets = ["Leo", "George", "Mulligan", "Freddy"]
     const actualPets: string[] = []
+
     const madisonRows = page.getByRole('row', { name: 'Madison' })
 
 
     for (const row of await madisonRows.all()) {
-      const petNameOfOwner = await row.locator('td').last().textContent()!
-      actualPets.push(petNameOfOwner.trim())
+      const petNameOfOwner = await row.locator('td').last().textContent()
+      actualPets.push(petNameOfOwner?.trim()!)
     }
     expect(actualPets).toEqual(expectedPets)
   })
@@ -101,8 +102,8 @@ test('Validate specialty lists', async ({ page }) => {
   const dropdownSpecialties = page.locator('.dropdown-content label')
   const dropdownSpecialtiesArray: string[] = []
   for (const row of await dropdownSpecialties.all()) {
-    const value = await row.textContent()!
-    dropdownSpecialtiesArray.push(value)
+    const value = await row.textContent()
+    dropdownSpecialtiesArray.push(value!)
   }
   expect(dropdownSpecialtiesArray).toEqual(specialties)
   await page.getByRole('checkbox', { name: 'oncology' }).check()
