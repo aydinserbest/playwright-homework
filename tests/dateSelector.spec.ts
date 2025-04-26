0c7fee73b97707d3d245d7b345d6d5be3786b4c8
aydinserbest <aydinserbest34@gmail.com>
2025-04-26 14:35:40 +0200

    Add test for adding and deleting visits

---
+++ tests/dateSelector.spec.ts
@@ -0,0 +1,104 @@
+import { test, expect, Page } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('Owners').click()
+    await page.getByText('Search').click()
+})
+
+test('Select the desired date in the calendar', async ({ page }) => {
+    await page.getByRole('link', { name: 'Harold Davis' }).click()
+    await page.getByRole('button', { name: 'Add New Pet' }).click()
+    await page.waitForResponse('**/pettypes')
+    await page.getByLabel('Name').fill('Tom')
+    await expect(page.locator('.form-group', { hasText: 'Name' }).locator('.form-control-feedback')).toHaveClass(/.*glyphicon-ok.*/)
+    await page.getByRole('button', { name: 'Open calendar' }).click()
+    await page.getByRole('button', { name: 'Choose month and year' }).click()
+    await page.getByRole('button', { name: 'Previous 24 years' }).click()
+    await page.getByRole('gridcell', { name: '2014' }).click()
+    await page.getByRole('button', { name: '05 2014' }).click()
+    await page.getByRole('button', { name: '2014/05/02' }).click()
+    await expect(page.locator('input[name="birthDate"]')).toHaveValue('2014/05/02')
+    await page.getByLabel('Type').selectOption('dog')
+    await page.getByRole('button', { name: 'Save Pet' }).click()
+    const targetPet = page.locator('app-pet-list').nth(1)
+    await expect(targetPet).toContainText('Tom')
+    await expect(targetPet).toContainText('2014-05-02')
+    await expect(targetPet).toContainText('dog')
+    await page.locator('app-pet-list', { hasText: 'Tom' }).getByRole('button', { name: 'Delete Pet' }).click()
+    await expect(page.getByText('Tom')).toHaveCount(0)
+})
+test('Select the dates of visits and validate dates order', async ({ page }) => {
+    await page.getByRole('link', { name: 'Jean Coleman' }).click()
+    await page.locator('app-pet-list', { hasText: 'Samantha' }).getByRole('button', { name: 'Add Visit' }).click()
+    await expect(page.getByRole('heading', { name: 'New Visit' })).toBeVisible()
+    const petDataRow = page.locator('.table-striped tr').nth(1)
+    await expect(petDataRow.locator('td').nth(0)).toHaveText('Samantha')
+    await expect(petDataRow.locator('td').nth(3)).toHaveText('Jean Coleman')
+    await page.getByRole('button', { name: 'Open calendar' }).click()
+    await page.locator('.mat-calendar-body-active').click()
+    const todayDate = new Date()
+    const expectedYear = todayDate.getFullYear()
+    const expectedMonth = String(todayDate.getMonth() + 1).padStart(2, '0')
+    const expectedDay = String(todayDate.getDate()).padStart(2, '0')
+    const expectedVisitDateSlashFormat = `${expectedYear}/${expectedMonth}/${expectedDay}`
+    await expect(page.locator('input[name="date"]')).toHaveValue(expectedVisitDateSlashFormat)
+    await page.locator('#description').fill('dermatologists visit')
+    await page.getByRole('button', { name: 'Add Visit' }).click()
+    const expectedVisitDateDashFormat = `${expectedYear}-${expectedMonth}-${expectedDay}`
+    const visitRow = page
+  .locator('app-pet-list', { hasText: 'Samantha' })
+  .locator('.table-condensed tr')
+    await expect(visitRow.nth(1).locator('td').first()).toHaveText(expectedVisitDateDashFormat)
+
+    await page.locator('app-pet-list', { hasText: 'Samantha' }).getByRole('button', { name: 'Add Visit' }).click()
+    await page.getByRole('button', { name: 'Open calendar' }).click()
+    const fortyFiveDaysAgoDate = new Date(todayDate)
+    fortyFiveDaysAgoDate.setDate(todayDate.getDate() - 45)
+    const expectedPastYear = fortyFiveDaysAgoDate.getFullYear()
+    const expectedPastMonth = (fortyFiveDaysAgoDate.getMonth() + 1).toString().padStart(2, '0')
+    const expectedPastDay = fortyFiveDaysAgoDate.getDate().toString()
+
+    const expectedPastVisitDateDashFormat = `${expectedPastYear}-${expectedPastMonth}-${expectedPastDay}`
+    const expectedMonthYearText = `${expectedPastMonth} ${expectedPastYear}`
+    let calendarMonthYear = await page.locator('.mat-calendar-period-button').textContent()
+    while (!calendarMonthYear?.includes(expectedMonthYearText)) {
+        await page.getByRole('button', { name: 'Previous month' }).click()
+        calendarMonthYear = await page.locator('.mat-calendar-period-button').textContent()
+
+    }
+    await page.getByRole('button', { name: expectedPastDay }).click()
+    await page.locator('#description').fill('massage therapy')
+    await page.getByRole('button', { name: 'Add Visit' }).click()
+
+    const firstVisitDateText = (await visitRow
+        .nth(1)
+        .locator('td')
+        .first()
+        .textContent())!
+
+    const secondVisitDateText = (await visitRow
+        .nth(2)
+        .locator('td')
+        .first()
+        .textContent())!
+
+    const firstVisitDateSamantha = new Date(firstVisitDateText)
+    const secondVisitDateSamantha = new Date(secondVisitDateText)
+
+    expect(firstVisitDateSamantha > secondVisitDateSamantha).toBeTruthy()
+    await visitRow
+        .nth(1)
+        .locator('button', { hasText: 'Delete Visit' })
+        .click()
+    await visitRow
+        .nth(2)
+        .locator('button', { hasText: 'Delete Visit' })
+        .click()
+    await expect(
+        page.locator('app-pet-list', { hasText: 'Samantha' }).locator('.table-condensed')
+    ).not.toContainText(expectedVisitDateDashFormat)
+    await expect(
+        page.locator('app-pet-list', { hasText: 'Samantha' }).locator('.table-condensed')
+    ).not.toContainText(expectedPastVisitDateDashFormat)
+})
